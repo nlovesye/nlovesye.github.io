efd3ec68b2095ceb78beb7e8d93e734ada2aea78
nlovesye <894765963@qq.com>
2019-02-26 18:22:28 +0800

    dev

---
+++ precache-manifest.2cd581c398904881b782f8c7032740bb.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = [
+  {
+    "revision": "1943590632be2e5914c4",
+    "url": "/css/app.ccce2e5a.css"
+  },
+  {
+    "revision": "1943590632be2e5914c4",
+    "url": "/js/app.deb703b8.js"
+  },
+  {
+    "revision": "378ca876efae9bfd4200",
+    "url": "/css/chunk-vendors.79e904ab.css"
+  },
+  {
+    "revision": "378ca876efae9bfd4200",
+    "url": "/js/chunk-vendors.c62d8d51.js"
+  },
+  {
+    "revision": "9150032537b79dcbf149",
+    "url": "/css/login.72540daf.css"
+  },
+  {
+    "revision": "9150032537b79dcbf149",
+    "url": "/js/login.be78c8ca.js"
+  },
+  {
+    "revision": "baae5b5fce7d6c3cf435",
+    "url": "/js/login~公司法人~客户资料~费用项目~车型管理.7ad0807c.js"
+  },
+  {
+    "revision": "6331ec124a8d8202a4ed",
+    "url": "/css/公司法人.1c84ce2d.css"
+  },
+  {
+    "revision": "6331ec124a8d8202a4ed",
+    "url": "/js/公司法人.90fbbbfe.js"
+  },
+  {
+    "revision": "353a3d2ac6179be5ccd8",
+    "url": "/js/公司法人~客户资料~数据字典~费用项目~车型管理.7afd0143.js"
+  },
+  {
+    "revision": "8894b479cb0282691ee1",
+    "url": "/js/公司法人~客户资料~费用项目~车型管理.b271e9ce.js"
+  },
+  {
+    "revision": "8d622cf7278046660a04",
+    "url": "/css/单位配置.c5eb09a1.css"
+  },
+  {
+    "revision": "8d622cf7278046660a04",
+    "url": "/js/单位配置.5ec35b61.js"
+  },
+  {
+    "revision": "5c0eba227bf00701dfe7",
+    "url": "/css/地址档案.824bbfdb.css"
+  },
+  {
+    "revision": "5c0eba227bf00701dfe7",
+    "url": "/js/地址档案.236977a4.js"
+  },
+  {
+    "revision": "ed85ef7a522f81dc7a41",
+    "url": "/css/客户资料.c01f40c7.css"
+  },
+  {
+    "revision": "ed85ef7a522f81dc7a41",
+    "url": "/js/客户资料.4423eb08.js"
+  },
+  {
+    "revision": "41883ac2bf77e212c162",
+    "url": "/css/数据字典.b98ca7f2.css"
+  },
+  {
+    "revision": "41883ac2bf77e212c162",
+    "url": "/js/数据字典.be7eb4af.js"
+  },
+  {
+    "revision": "e2730f4542336c4830f0",
+    "url": "/css/视图注册.3ad78729.css"
+  },
+  {
+    "revision": "e2730f4542336c4830f0",
+    "url": "/js/视图注册.53caeafe.js"
+  },
+  {
+    "revision": "4868778b8abcb7b11350",
+    "url": "/css/费用项目.97de19b6.css"
+  },
+  {
+    "revision": "4868778b8abcb7b11350",
+    "url": "/js/费用项目.1d79e457.js"
+  },
+  {
+    "revision": "bdf4a7f5e39ee3a151bc",
+    "url": "/css/车型管理.e4f6a40e.css"
+  },
+  {
+    "revision": "bdf4a7f5e39ee3a151bc",
+    "url": "/js/车型管理.9355c3dc.js"
+  },
+  {
+    "revision": "bc11fb32c3761d859867",
+    "url": "/css/首页.b5e89cec.css"
+  },
+  {
+    "revision": "bc11fb32c3761d859867",
+    "url": "/js/首页.159422b2.js"
+  },
+  {
+    "revision": "99ac3308dd8ee14f749f51538d0d5b9e",
+    "url": "/fonts/ionicons.99ac3308.woff"
+  },
+  {
+    "revision": "d535a25a79fb1365ae814b61e88fae71",
+    "url": "/fonts/ionicons.d535a25a.ttf"
+  },
+  {
+    "revision": "17bf2ac0d7ccdaf207cc53c070d222b4",
+    "url": "/img/ionicons.17bf2ac0.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "a2c4a261a239aa84463dc70e4bac9b9a",
+    "url": "/img/ionicons.a2c4a261.svg"
+  },
+  {
+    "revision": "db657b0644d7ec22dc95bd304cd75c67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+];
