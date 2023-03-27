7adefc49702b38d6fa86c5dcdf485db37f1424f3
污锁谱 <894765963@qq.com>
2023-03-27 16:14:19 +0800

    fix: deploy

--- dist/assets/index-d27d993c.js
+++
@@ -1,1 +0,0 @@
-import{d as o}from"./index-019b45cb.js";const s="ns-src-views-Blog-index-module_blog",l={blog:s};function t(){return o("div",{className:l.blog,children:"this is blog module"})}export{t as default};
--- vite.config.ts
+++ vite.config.ts
@@ -37,6 +37,7 @@ export default defineConfig({
   },
   server,
   build: {
-    outDir: '../dist',
+    outDir: '..',
+    assetsDir: './assets',
   },
 });
