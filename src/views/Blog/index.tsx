10ebc6202ddaaf13d5b893eee68c64f5db6d3bf2
🌏污锁谱 <894765963@qq.com>
2023-06-14 23:04:22 +0800

    feat: 'bmap'

--- src/views/Blog/index.tsx
+++ src/views/Blog/index.tsx
@@ -1,5 +1,15 @@
+import { MapApiLoaderHOC, Map, Marker, NavigationControl, InfoWindow } from 'react-bmapgl';
+
 import styles from './index.module.less';
 
-export default function Blog() {
-  return <div className={styles.blog}>this is blog module</div>;
-}
+export default MapApiLoaderHOC({ ak: 'xbsoCabGU9I8fD1XUIomFvpob8t2fsGU' })(function Blog() {
+  return (
+    <div className={styles.blog}>
+      <Map center={{ lng: 116.402544, lat: 39.928216 }} zoom="11">
+        <Marker position={{ lng: 116.402544, lat: 39.928216 }} />
+        <NavigationControl />
+        <InfoWindow position={{ lng: 116.402544, lat: 39.928216 }} text="内容" title="标题" />
+      </Map>
+    </div>
+  );
+});
